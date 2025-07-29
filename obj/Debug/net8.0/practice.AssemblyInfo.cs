6430fd5ef509fa67f79b49490da9be117bafdcd4
Christain Aranez Nemeno <christiannemeno80@gmail.com>
2025-07-29 09:43:43 +0800

    yy

--- obj/Debug/net8.0/practice.AssemblyInfo.cs
+++ obj/Debug/net8.0/practice.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("practice")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+248f6e19b687aa3cda41fb9e08abaea94fbe2b9e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b11ce1c96ea48ddfc816229061318f29310534f6")]
 [assembly: System.Reflection.AssemblyProductAttribute("practice")]
 [assembly: System.Reflection.AssemblyTitleAttribute("practice")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
