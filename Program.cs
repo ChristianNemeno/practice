b11ce1c96ea48ddfc816229061318f29310534f6
Christain Aranez Nemeno <christiannemeno80@gmail.com>
2025-07-29 09:43:03 +0800

    even number

--- Program.cs
+++ Program.cs
@@ -1,2 +1,23 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+bool EvenChecker(int a)
+{
+    return a % 2 == 0;
+}
+
+Console.Write("Enter a number ");
+string? input = Console.ReadLine();
+
+if (int.TryParse(input, out int number))
+{
+    if (EvenChecker(number))
+    {
+        Console.WriteLine($"{number} is an even number.");
+    }
+    else
+    {
+        Console.WriteLine($"{number} is not an even number.");
+    }
+}
+
+
+
